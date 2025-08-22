03a97e2dc53a3b4d7f98256ee21cfeaeec055b74
mekachi015 <katlegomakoti@gmail.com>
2025-08-22 07:10:18 +0200

    Dockerized ends of application

--- Vue Frontend/portfolio/vue.config.js
+++ Vue Frontend/portfolio/vue.config.js
@@ -1,27 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+
+
 module.exports = defineConfig({
   transpileDependencies: true,
-
   devServer: {
-    host: '192.168.0.151',
     port: 8080,
-    allowedHosts: 'all',
     proxy: {
-      '/api' : {
-        target: "http://localhost:8081", // Backend server URL
+      '^/api': {
+        target: 'http://localhost:8081',
         changeOrigin: true,
-        secure: false,
-        logLevel: 'debug',
-        pathRewrite: {
-          '^/api': '/api' // Remove /api prefix when forwarding to backend
-        },
-        onProxyReq: (proxyReq, req, res) => {
-          console.log('Proxying request:', req.url);
-
-          proxyReq.setHeader('Origin', 'http://localhost:8080')
-        }
+        secure: false
       }
     }
   }
-
 })
+  
